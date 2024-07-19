b41e404f66dfabfe035fbd8fb695b3fad77b5fb8
David Heath <david.heath@digital.cabinet-office.gov.uk>
2024-07-19 18:23:51 +0100

    tweaks

--- check_swimming_slots.rb
+++ check_swimming_slots.rb
@@ -17,26 +17,39 @@ def fetch_json(url)
   JSON.parse(response.body)
 end
 
-# Parse the JSON data and identify bookable slots within the given time range
-def find_bookable_slots(data, start_time, end_time)
-  bookable_slots = []
+class Slot
+  attr_reader :data
 
-  data['data'].each do |slot|
-    slot_start_time = Time.parse("#{slot['date']['raw']} #{slot['starts_at']['format_24_hour']}")
-    slot_end_time = Time.parse("#{slot['date']['raw']} #{slot['ends_at']['format_24_hour']}")
-
-    if slot['action_to_show']['status'] == 'BOOK' && slot_start_time >= start_time && slot_end_time <= end_time
-      bookable_slots << {
-        name: slot['name'],
-        start_time: slot['starts_at']['format_12_hour'],
-        end_time: slot['ends_at']['format_12_hour'],
-        location: slot['location_name'],
-        spaces_remaining: slot['spaces_remaining']
-      }
-    end
+  def initialize(data)
+    @data = data
   end
 
-  bookable_slots
+  def bookable?
+    data['action_to_show']['status'] == 'BOOK'
+  end
+
+  def slot_start_time
+    Time.parse("#{data['date']['raw']} #{data['starts_at']['format_24_hour']}")
+  end
+
+  def in_range?(range_start, range_end)
+    range = Time.parse(range_start)..Time.parse(range_end)
+    range.cover?(slot_start_time)
+  end
+
+  def name; data['name']; end
+  def start_time; data['starts_at']['format_12_hour']; end
+  def end_time; data['ends_at']['format_12_hour']; end
+  def location; data['location_name']; end
+  def spaces_remaining; data['spaces_remaining']; end
+end
+
+# Parse the JSON data and identify bookable slots within the given time range
+def find_bookable_slots(data, start_time, end_time)
+  data['data']
+    .map {|d| Slot.new(d)}
+    .select(&:bookable?)
+    .select {|s| s.in_range?(start_time, end_time) }
 end
 
 # Send a message via Telegram
@@ -46,32 +59,35 @@ def send_telegram_message(token, chat_id, message)
   end
 end
 
-# URL of the JSON file
-url = 'https://better-admin.org.uk/api/activities/venue/london-fields-lido/activity/swimmingft/times?date=2024-07-20'
-
-# Fetch and parse JSON data
-data = fetch_json(url)
-
-# Define the time range (example: from 6:00am to 8:00am)
 require 'date'
 require 'time'
 tomorrow = Date.today+1
 
-start_time = DateTime.new(tomorrow.year, tomorrow.month, tomorrow.day, 8, 0, 0)
-end_time = DateTime.new(tomorrow.year, tomorrow.month, tomorrow.day, 14, 0, 0)
+# URL of the JSON file
+url = "https://better-admin.org.uk/api/activities/venue/london-fields-lido/activity/swimmingft/times?date=#{tomorrow.to_s}"
+
+# Fetch and parse JSON data
+data = fetch_json(url)
+
+start_time = "08:00"
+end_time = "14:00"
 
 # Find bookable slots within the specified time range
 bookable_slots = find_bookable_slots(data, start_time, end_time)
 
 # Prepare and send the Telegram message if there are bookable slots
 if bookable_slots.any?
-  message = "Bookable swimming slots between #{start_time} and #{end_time} tomorrow:\n"
+  puts "#{bookable_slots.size} slots available on #{tomorrow.to_s} between #{start_time} and #{end_time}"
+  
+  message = "Bookable swimming slots between #{start_time} and #{end_time}:\n"
   bookable_slots.each do |slot|
-    message += "#{slot[:name]} from #{slot[:start_time]} to #{slot[:end_time]} at #{slot[:location]}, Spaces remaining: #{slot[:spaces_remaining]}\n"
+    message += "#{slot.name} from #{slot.start_time} to #{slot.end_time} at #{slot.location}, Spaces remaining: #{slot.spaces_remaining}\n"
   end
 
   # Send the message via Telegram
   bot_token = ENV['BOT_TOKEN']
   chat_id = ENV['CHAT_ID']
   send_telegram_message(bot_token, chat_id, message)
+else
+  puts "No slots available on #{tomorrow.to_s} between #{start_time} and #{end_time}"
 end
